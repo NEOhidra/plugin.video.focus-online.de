1e20516a8373654c4c94f8be7b4c69de6dcb3bb9
bromix <bromix@gmx.net>
2015-03-14 17:47:14 +0100

    resolves #2

--- resources/lib/focus/provider.py
+++ resources/lib/focus/provider.py
@@ -11,7 +11,8 @@ __author__ = 'bromix'
 class Provider(kodion.AbstractProvider):
     def __init__(self):
         kodion.AbstractProvider.__init__(self)
-        self._local_map.update({'focus.related': 30500})
+        self._local_map.update({'focus.related': 30500,
+                                'focus.all-videos': 30501})
         self._client = None
         pass
 
@@ -89,8 +90,23 @@ class Provider(kodion.AbstractProvider):
         json_data = client.get_videos_from_data(json_data, category)
         return self._do_videos(context=context, json_data=json_data)
 
+    @kodion.RegisterProviderPath('^/all-videos/$')
+    def _on_all_videos(self, context, re_match):
+        context.set_content_type(kodion.constants.content_type.EPISODES)
+        client = self.get_client(context)
+        json_data = context.get_function_cache().get(FunctionCache.ONE_HOUR, client.get_root_data)
+        json_data = client.get_all_videos(json_data)
+        return self._do_videos(context=context, json_data=json_data)
+
     def on_root(self, context, re_match):
         result = []
+
+        # all videos
+        all_videos_item = DirectoryItem('[B]'+context.localize(self._local_map['focus.all-videos'])+'[/B]',
+                                        context.create_uri(['all-videos']))
+        all_videos_item.set_image(context.create_resource_path('media', 'category.png'))
+        all_videos_item.set_fanart(self.get_fanart(context))
+        result.append(all_videos_item)
 
         # categories
         client = self.get_client(context)
